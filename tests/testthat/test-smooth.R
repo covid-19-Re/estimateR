e04977dcf198847780fe9064bdedcf54b43d4da7
Jérémie Scire <jeremie.scire@bsse.ethz.ch>
2021-03-23 12:19:21 +0100

    Work on unit tests

--- tests/testthat/test-smooth.R
+++ tests/testthat/test-smooth.R
@@ -17,7 +17,17 @@ test_that("smooth_incidence output values are positive ", {
          expect_gte(min(smoothed_values), 0)})
 })
 
+test_that("smooth_incidence output values sum up to the total of the original incidence ", {
+  random_values <- sample.int(1000, size = 100, replace= TRUE)
+
+  sapply(smoothing_methods_tested, function(x) {
+    smoothed_values <- .get_values(smooth_incidence(random_values, smoothing_method = x))
+    expect_equal(sum(smoothed_values), sum(random_values))})
+})
+
 test_that("smooth_incidence output values are constant if input is constant ", {
+  #TODO unskip test when added way to deal with left-truncated incidence input
+  skip("Left-truncated incidence input cannot be dealt with yet.")
   constant_values <- rep(5.3, times = 100)
 
   sapply(smoothing_methods_tested, function(x) {
@@ -25,10 +35,18 @@ test_that("smooth_incidence output values are constant if input is constant ", {
     expect_equal(smoothed_values, constant_values)})
 })
 
-test_that("smooth_incidence output values sum up to the total of the original incidence ", {
+test_that("smooth_incidence output values are bounded by bounds of noisy values", {
   random_values <- sample.int(1000, size = 100, replace= TRUE)
 
   sapply(smoothing_methods_tested, function(x) {
     smoothed_values <- .get_values(smooth_incidence(random_values, smoothing_method = x))
-    expect_equal(sum(smoothed_values), sum(random_values))})
+    expect_gte(min(smoothed_values), min(random_values))
+    expect_lte(max(smoothed_values), max(random_values))})
+})
+
+test_that("smooth_incidence output stays consistent for LOESS method", {
+  noisy_values <- c(272, 78, 859, 642, 411, 612, 192, 262, 399, 371, 69, 80, 221, 945, 198, 896, 705, 155, 498, 795)
+  ref_smoothed_values <- c(213.481,238.774,265.581,293.119,317.855,341.012,365.157,388.764,410.31,430.617,451.25,471.793,491.829,511.203,530.235,549.217,568.446,587.809,607.097,626.452)
+  smoothed_values <- .get_values(smooth_incidence(noisy_values, smoothing_method = "LOESS"))
+  expect_equal(smoothed_values, ref_smoothed_values, tolerance = 1E-3)
 })
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,32 +1,34 @@
 #TODO test that merge outputs works with inputs of
 
-test_that("get_module_input() deals with well-formatted input", {
+test_that(".get_module_input() deals with well-formatted input", {
   toy_incidence <- c(1,2,1,0)
   data_1 <- list(values = toy_incidence, index_offset = -4)
 
-  expect_identical(get_module_input(toy_incidence), list(values = toy_incidence, index_offset = 0))
-  expect_identical(get_module_input(data_1), data_1)
+  expect_identical(.get_module_input(toy_incidence), list(values = toy_incidence, index_offset = 0))
+  expect_identical(.get_module_input(data_1), data_1)
 })
 
-test_that("get_module_input() checks input format", {
+test_that(".get_module_input() checks input format", {
   toy_incidence <- c(1,23,1,-50)
   data_1 <- list(values = toy_incidence, index_offset = -2, baz = 6)
   invalid_input_1 <- list(a = toy_incidence, b = -2, c = 6)
 
-  expect_identical(get_module_input(data_1), list(values = toy_incidence, index_offset = -2))
-  expect_error(get_module_input(invalid_input_1), "Wrong input format.")
+  expect_identical(.get_module_input(data_1), list(values = toy_incidence, index_offset = -2))
+  skip("Input format is not validated yet.")
+  #TODO unskip when input is validated
+  expect_error(.get_module_input(invalid_input_1), "Wrong input format.")
 })
 
-test_that("get_module_output() deals with well-formatted input", {
+test_that(".get_module_output() deals with well-formatted input", {
   results <- c(1,2,3,-2,0,0)
   input_1 <- list(values = c(1,1,1,1,1), index_offset = 0)
   input_2 <- list(values = c(1,1,1,1,1), index_offset = -1)
 
-  expect_identical(get_module_output(results, input_1), results)
-  expect_identical(get_module_output(results, input_2), list(values = results, index_offset = -1))
-  expect_identical(get_module_output(results, input_2, offset = 3), list(values = results, index_offset = 2))
+  expect_identical(.get_module_output(results, input_1), results)
+  expect_identical(.get_module_output(results, input_2), list(values = results, index_offset = -1))
+  expect_identical(.get_module_output(results, input_2, offset = 3), list(values = results, index_offset = 2))
 })
 
-test_that("get_module_output() checks input format", {
+test_that(".get_module_output() checks input format", {
   #TODO make test
 })
