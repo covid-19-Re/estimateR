51b97707d7198ee9440426231ce299fc1ba2a812
Ana Grosu <agrosu@student.ethz.ch>
2021-08-01 19:36:52 +0200

    changed some test values after merge

--- R/smooth.R
+++ R/smooth.R
@@ -53,35 +53,13 @@ smooth_incidence <- function(incidence_data,
 #' @inheritParams inner_module
 #' @param data_points_incl integer. Size of the window used in the LOESS algorithm.
 #' @param degree integer. LOESS degree.
+#' @param initial_Re_estimate_window integer. In order to help with the smoothing, the function extends
+#' the data back in time, padding with values obtained by assuming a constant Re. This parameter represents 
+#' the number of timesteps in the beginning of \code{incidence_input} to take into account when computing
+#' the average initial Re.
 #'
 #' @return module output object. Smoothed incidence.
 .smooth_LOESS <- function(incidence_input, data_points_incl = 21, degree = 1, initial_Re_estimate_window = 5) {
-
-  .are_valid_argument_values(list(list(incidence_input, "module_input"),
-                                  list(data_points_incl, "non_negative_number"),
-                                  list(degree, "non_negative_number"))) #minimal test; needs to be one of {0,1,2}, but stats::loess already throws if it isn't
-
-  incidence_vector <- .get_values(incidence_input)
-
-  n_points <- length(incidence_vector)
-  sel_span <- data_points_incl / n_points
-
-  n_pad <- round(length(incidence_vector) * sel_span * 0.5)
-
-  c_data <- data.frame(value = c(rep(0, n_pad), incidence_vector),
-                       date_num = 1:(n_pad + n_points))
-
-  c_data.lo <- stats::loess(value ~ date_num, data = c_data, span = sel_span, degree = degree)
-  smoothed <- stats::predict(c_data.lo)
-  smoothed[smoothed < 0] <- 0
-  raw_smoothed_counts <- smoothed[(n_pad + 1):length(smoothed)]
-  normalized_smoothed_counts <-
-    raw_smoothed_counts * sum(incidence_vector, na.rm = T) / sum(raw_smoothed_counts, na.rm = T)
-
-  return(.get_module_output(normalized_smoothed_counts, incidence_input))
-}
-
-.smooth_TEST4 <- function(incidence_input, data_points_incl = 21, degree = 1, initial_Re_estimate_window = 5) {
   
   .are_valid_argument_values(list(list(incidence_input, "module_input"),
                                   list(data_points_incl, "non_negative_number"),
